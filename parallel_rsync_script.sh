a29de3f8726da9217d82c8423f0c9e3194ab8f37
David Simpson <31688862+ds-04@users.noreply.github.com>
2022-09-06 11:29:15 +0100

    Update parallel_rsync_script.sh

--- parallel_rsync_script.sh
+++ parallel_rsync_script.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+#Script to launch an rsync instance, called from parallel_rsync_launcher_template.sh
+
+if [ "$#" -ne 2 ]; then
+echo "You must enter exactly 2 command line arguments - Full path to a directory and the BACKUP server IP and in that order"
+exit 1
+fi
+
+#Get the date for this run
+DATE_OF_RUN=`date +%F_%H_%M`
+
+# base path for destination/backup
+BACKUP_PATH="/backup/destination/on/remote/"    #note includes trailing slash here
+
+#INPUT TO SCRIPT
+#local full path to a dir
+DLINE=$1
+BACKUP_SERVER_IP=$2
+
+#Get the dir line so it can be logged
+DIR_LOG_LINE=`/usr/bin/echo ${DLINE}`  #in this form, it does nothing, could be changed to extract something of interest via a pipe
+
+/usr/bin/rsync -e "ssh -i /path/to/key" -avz --acls --exclude=".exclude_something_example" --delete-after ${DLINE} user@${BACKUP_SERVER_IP}:/${BACKUP_PATH} > /dev/null 2>"/var/log/rsync/rsync_log_${DIR_LOG_LINE}_${DATE_OF_RUN}.txt"
