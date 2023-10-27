92bbb08c6fd73930e6b38065102033837bf915cc
David Simpson <31688862+ds-04@users.noreply.github.com>
2023-10-27 13:28:58 +0100

    Update parallel_rsync_launcher_template.sh
    launcher script improvements

--- parallel_rsync_launcher_template.sh
+++ parallel_rsync_launcher_template.sh
@@ -2,12 +2,14 @@
 
 #------Change this------ 
 #export the path to a dir containing file list if necessary, here it is needed to see parallel_rsync_script.sh
-#export PATH=/rsync:${PATH}
+#which for this example has been located inside /root/rsync along with the INPUT_FILE file list
+export PATH=/root/rsync:${PATH}
 
 #list contents should be dirs not files, meaning rsync launched per dir entry NOT file
 #------Change this------ 
-INPUT_FILE=/rsync/list_of_dirs_to_copy 
+INPUT_FILE=/root/rsync/list_of_dirs_to_copy 
 
+#OPTIONAL
 #------Change this------ activate if you want to check the host you are running from
 #check where this script is running from
 #HOST_SELF=`hostname`
@@ -18,11 +20,12 @@ INPUT_FILE=/rsync/list_of_dirs_to_copy
 # exit 1
 #fi
 
-#IP/dest from supplied to script
+#IP is supplied to this script
+#if the rsync is between two local mount points you won't need this and should remove within the final line also
 BACKUP_SERVER_IP="${1}"
 
-#------Change this------ path to parallel binary
-parallel="/parallel/bin/path/parallel"
+#------Check this------ path to parallel binary
+parallel="/usr/bin/parallel"
 
 #Get the date
 DATE_OF_RUN=`date +%F_%H_%M`
@@ -30,4 +33,4 @@ DATE_OF_RUN=`date +%F_%H_%M`
 #Call another script to run in parallel...
 #N=1 arguement
 #j=2 jobs (2 rsyncs)
-${parallel} -N1 -j2 --joblog /var/log/rsync/rsync_home_${DATE_OF_RUN}_logfile.parallel parallel_rsync_script.sh {} ${BACKUP_SERVER_IP} ::: `cat ${INPUTE_FILE}`
+${parallel} -N1 -j2 --joblog /var/log/rsync/rsync_transfer_${DATE_OF_RUN}_logfile.parallel parallel_rsync_script.sh {} ${BACKUP_SERVER_IP} ::: `cat ${INPUT_FILE}`
